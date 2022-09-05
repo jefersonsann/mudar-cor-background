991842dc1d2b7b7b273674812033fdd0b756d5a1
Jeferson Santos <jefersonsann@hotmail.com>
2022-09-04 22:55:11 -0300

    Pagina simples para pegar cor aleatoria

---
+++ js/hex.js
@@ -0,0 +1,27 @@
+// Array com os valores a serem ramdomizados
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+// Pegando botão e texto da cor
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Logica ao click botão
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let hexColor = '#';
+
+  // Loop para pegar 6 items do array
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // Colocando o valor retornado como texto
+  color.textContent = hexColor;
+  // Colocando o valor como cor do texto
+  color.style.color = hexColor;
+  // Colocando o valor retornado no Bg body
+  document.body.style.backgroundColor = hexColor;
+});
+
+// Pegando os 6 irems no array randomicamente
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * hex.length);
+};
---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Array com cores
+const colors = [
+  'green',
+  'red',
+  'blue',
+  '#FFFFFF',
+  '#F15025',
+  'rgb(248, 159, 38)',
+  'rgb(221, 146, 76)',
+];
+// Pegando botão e texto da cor
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const randomNumber = getRandomNumber();
+
+  // Colocando o valor retornado do array no Bg body
+  document.body.style.backgroundColor = colors[randomNumber];
+  // Colocando o valor retornado do array como texto
+  color.textContent = colors[randomNumber];
+  // Colocando o valor como cor do texto
+  color.style.color = colors[randomNumber];
+});
+
+// Pegando 1 item aleatorio no array
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
